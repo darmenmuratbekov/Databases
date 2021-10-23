fc85b258e19813c8ca0fab52b8868c0d77e11ea6
Darmen <75259056+darmenmuratbekov@users.noreply.github.com>
2021-10-23 14:03:09 +0600

    Create 1example.sql

---
+++ lecture4/1example.sql
@@ -0,0 +1,21 @@
+create database wwww;
+
+
+
+create table messs
+(
+    id  serial,
+    name  varchar(20),
+    dept_name varchar(20),
+    salary  integer
+);
+
+insert into  messs (id,  name, dept_name, salary)
+VALUES ('20000','darmen',  'kkkk',  10000);
+VALUES ('200000','k', 'messi' ,  100000)
+
+
+select * from messs;
+
+
+
